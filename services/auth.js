0c948a11edad9798c0379f88aad69836bd0ce6f5
twolff-makor <twolff@makor-capital.com>
2023-06-05 09:54:21 +0300

    removed comment

--- services/auth.js
+++ services/auth.js
@@ -4,21 +4,6 @@ const URL = process.env.REST_URL;
 const USERNAME = process.env.UAT_USERNAME;
 const PASSWORD = process.env.UAT_PASSWORD;
 
-// async function getToken() {
-//     const response = await axios.put(`https://uat.rest-api.enigma-x.io/auth`, {
-//                                     "username": USERNAME,
-//                                     "password": PASSWORD
-//                             })
-//                             .catch(error => {
-//                                 console.error(error);
-//                             });
-//     const token = JSON.stringify(response.data.token);
-//     return token;
-// }
-
-// module.exports = { getToken };
-
-
 function getToken() {
     return new Promise((resolve, reject) => {
       axios
